530ec0e86370d98d6f4c3ec8915e8c1678b1a2fb
Lucie <luciebrochet358@gmail.com>
2020-06-18 13:48:59 +0000

    ajout icon back office

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -53,7 +53,13 @@
 
     <main>
         <?= $this->renderSection('content') ?>
+        <div class="content_admin">
+            <?= $this->renderSection('content_admin') ?>
+        </div>
     </main>
+    <footer>
+        <p>Réalisé par Emeric Durdos & Lucie Brochet - DU WEB - 2020</p>
+    </footer>
 </body>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
