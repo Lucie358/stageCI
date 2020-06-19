6d191abf79035f07ef858a1e25e3f34612f95f5f
emeric.durdos <emeric.durdos@gmail.com>
2020-06-19 12:53:50 +0000

    Gestion du visiteur sur le main controller

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -3,12 +3,14 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
+use App\Models\UserModel;
 
 class MainController extends BaseController
 {
 
     public function siteMap()//Mentions légales
     {
+        $this->sessionCheck();
         $data = [
             'title' => 'Plan du site'
         ];
@@ -17,10 +19,20 @@ class MainController extends BaseController
 
     public function legalNotices()//Plan du site
     {
+        $this->sessionCheck();
         $data = [
             'title' => 'Mentions légales'
         ];
 
         return view('main/legalNotices.php', $data);
     }
+
+    public function sessionCheck()
+	{
+		if (!isset($_SESSION['userData'])) {
+			$userModel = new UserModel();
+			$userInfos = $userModel->getUserInfos('Visiteur');
+			$_SESSION['userData'] = clone $userInfos[0];
+		}
+	}
 }
